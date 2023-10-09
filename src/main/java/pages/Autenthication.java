4dd5cbd18c2f060eb0ff3a08d790af9c467540a0
Pedro Amaral <pamaral@codacy.com>
2023-10-09 18:42:42 +0100

    fixes some issues

--- src/main/java/pages/Autenthication.java
+++ src/main/java/pages/Autenthication.java
@@ -16,24 +16,24 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class Autenthication extends BasePage {
 
-    private static final String NAV_BAR_LOGIN_LINK = "login_link";
-    private static final String VALIDATE_LOGIN_PAGE = "Welcome_Back_login";
-    private static final String GOOGLE_BUTTON = "login_google";
-    private static final String SELECT_USER_NAME = "identifierId";
-    private static final String NEXT_BTN = "identifierNext";
-    private static final String SELECT_PASSWORD_TXT = "//*[@type=\"password\"]";
-    private static final String PASSWORD_NEXT_BTN = "passwordNext";
-    private static final String BITBUCKET_BTN = "login_bitbucket";
-    private static final String LOGIN_FORM = "login-form";
-    private static final String LOG_IN_WITH_GOOGLE = "//*[@class='google-login']";
-    private static final String GITHUB_BTN = "login_github";
-    private static final String GITHUB_LOGIN_FORM = "class=\"auth-form-body mt-3\"";
-    private static final String GITHUB_LOGIN_TXT = "login_field";
-    private static final String GITHUB_PASSWORD_TXT = "password";
-    private static final String GITHUB_SUBMIT = "commit";
-    private static final String SELECT_ACCOUNT = "account-dropdown";
-    private static final String SIGN_OUT_BTN = "signout-form";
-    private static final String TESTE = "//*[@class='ANuIbb IdAqtf']";
+    public static final String NAV_BAR_LOGIN_LINK = "login_link";
+    public static final String VALIDATE_LOGIN_PAGE = "Welcome_Back_login";
+    public static final String GOOGLE_BUTTON = "login_google";
+    public static final String SELECT_USER_NAME = "identifierId";
+    public static final String NEXT_BTN = "identifierNext";
+    public static final String SELECT_PASSWORD_TXT = "//*[@type=\"password\"]";
+    public static final String PASSWORD_NEXT_BTN = "passwordNext";
+    public static final String BITBUCKET_BTN = "login_bitbucket";
+    public static final String LOGIN_FORM = "login-form";
+    public static final String LOG_IN_WITH_GOOGLE = "//*[@class='google-login']";
+    public static final String GITHUB_BTN = "login_github";
+    public static final String GITHUB_LOGIN_FORM = "class=\"auth-form-body mt-3\"";
+    public static final String GITHUB_LOGIN_TXT = "login_field";
+    public static final String GITHUB_PASSWORD_TXT = "password";
+    public static final String GITHUB_SUBMIT = "commit";
+    public static final String SELECT_ACCOUNT = "account-dropdown";
+    public static final String SIGN_OUT_BTN = "signout-form";
+    public static final String TESTE = "//*[@class='ANuIbb IdAqtf']";
 
 
     public Autenthication(RemoteWebDriver driver, Environment env) {
